68a10b3398c4bf6ff61c2b2f295a954aec42d87e
Murtaza khan <murtazabakht66@gmail.com>
2026-05-05 01:40:38 +0500

    added new files

---
+++ 2 aiproject.py
@@ -0,0 +1,23 @@
+a = ("hello world ")
+print(a)
+
+a = [ 3,5,6,45,67,34,23,32,54,89,65,70]
+print(a)
+
+a = ("hello world")
+print(a)
+
+a = 2,34,54,64,754,75,
+print(a)
+
+a = 2.3, 6.4, 34.6
+print(a)
+
+a = None
+print(a) 
+
+a = True
+print(a)
+
+a = False
+print(a)
